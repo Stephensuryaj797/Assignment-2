502b62f6ed669d3b9270cbed41df0008ce9994dc
Stephensuryaj797 <146203803+Stephensuryaj797@users.noreply.github.com>
2023-10-01 23:31:24 +0530

    Add files via upload

---
+++ DictCreat.py
@@ -0,0 +1,6 @@
+dictionary={}
+for i in range(97,122+1):
+    m=chr(i)
+    dictionary[m]=ord(m)
+print(type(dictionary))
+print(dictionary)
---
+++ Tuple_sorting.py
@@ -0,0 +1,15 @@
+My_tup=[]
+n=int(input("Enter the number of tuples in the list: "))
+for i in range(n):
+    V=input("Enter tuple Ex: 2,3 (comma-separated values): ")
+    M=V.split(',')
+    tup=[]
+    for s in M:
+        tup.append(int(s))
+    My_tup.append(tuple(tup))
+print("Before Sorting",My_tup)
+for i in range(len(My_tup)):
+    for j in range(i+1,len(My_tup)):
+        if My_tup[i][-1]>My_tup[j][-1]:
+            My_tup[i],My_tup[j]=My_tup[j],My_tup[i]
+print("After Sorting",My_tup)
